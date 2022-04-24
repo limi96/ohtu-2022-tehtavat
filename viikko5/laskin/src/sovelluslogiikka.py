39343ffb92780548569e06acbbef069f25456eca
limi96 <mikelee199607@gmail.com>
2022-04-24 20:27:59 +0300

    Viikko5 teht5

--- viikko5/laskin/src/sovelluslogiikka.py
+++ viikko5/laskin/src/sovelluslogiikka.py
@@ -1,15 +1,30 @@
 class Sovelluslogiikka:
     def __init__(self, tulos=0):
         self.tulos = tulos
+        self.edellinen_luku_lista = []
+        self.edellinen_luku_lista.append(tulos)
+
+    def muista_edellinen_luku(self): 
+        self.edellinen_luku_lista.append(self.tulos)
+
+    def get_edellinen_luku(self):
+        listan_pituus = len(self.edellinen_luku_lista)
+        return self.edellinen_luku_lista[listan_pituus-1]
 
     def miinus(self, arvo):
+        self.muista_edellinen_luku()
         self.tulos = self.tulos - arvo
 
     def plus(self, arvo):
+        self.muista_edellinen_luku()
         self.tulos = self.tulos + arvo
 
     def nollaa(self):
         self.tulos = 0
 
+    def kumoa(self):
+        self.aseta_arvo(self.get_edellinen_luku())
+
     def aseta_arvo(self, arvo):
         self.tulos = arvo
+
